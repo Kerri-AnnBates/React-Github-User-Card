a3a18c6608e5b4c1f0aa3bb796b3988d15c7b48e
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-29 15:46:51 -0400

    fetching data..

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
-
+    
     this.state = {
       userData: [],
       followersData: []
     }
   }
+  
+  componentDidMount() {
+    axios.get('https://api.github.com/users/Kerri-AnnBates')
+    .then(resolve => {
+      // console.log(resolve.data);
+      this.setState({
+        userData: resolve.data
+      })
 
+    })
+    .catch(error => {
+      console.log('Error: Unable to fetch data', error);
+    })
+  }
+
+  
   render() {
+    
     return (
       <div className="App">
         <h1>Hello, User</h1>
